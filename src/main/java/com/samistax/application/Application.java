755de3ca8eba6e8c14a0b7b7f27250bb07fd4b47
samistax <sami.kaksonen@datastax.com>
2022-11-04 11:27:40 +0200

    Added Collaboration Engine licence property and configuration bean.

--- src/main/java/com/samistax/application/Application.java
+++ src/main/java/com/samistax/application/Application.java
@@ -1,14 +1,17 @@
 package com.samistax.application;
 
+import com.vaadin.collaborationengine.CollaborationEngineConfiguration;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
@@ -25,8 +28,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 public class Application implements AppShellConfigurator {
 
+    @Value("${vaadin.collaboration-engine.datadir:}")
+    private String CE_DATA_DIR;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CollaborationEngineConfiguration ceConfigBean() {
+        CollaborationEngineConfiguration configuration = new CollaborationEngineConfiguration(
+                licenseEvent -> {
+                    // See <<ce.production.license-events>>
+                });
+        configuration.setDataDir(CE_DATA_DIR);
+        return configuration;
+    }
 }
